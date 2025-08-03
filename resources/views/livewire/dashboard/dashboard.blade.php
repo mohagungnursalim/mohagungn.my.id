3c3bca0a59691adc405452fdef7233f7f8ea994f
mohagungnursalim <mohagungnursalim@gmail.com>
2025-08-03 10:15:07 +0800

    feat: hilangkan header di dashboard.blade

--- resources/views/livewire/dashboard/dashboard.blade.php
+++ resources/views/livewire/dashboard/dashboard.blade.php
@@ -1,14 +1,6 @@
 @extends('layouts.dashboard.main')
 @section('content')
-<div class="dark:bg-gray-800">
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    @for ($i = 0; $i < 4; $i++)
-        
+<div class="dark:bg-gray-800"> 
     <div class="py-12 bg-white dark:bg-gray-800 sm:rounded-lg">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-gray-100 dark:bg-gray-700 overflow-hidden shadow-sm sm:rounded-lg">
@@ -18,8 +10,6 @@
             </div>
         </div>
     </div>
-    @endfor
-
 </div> 
 @endsection
 
