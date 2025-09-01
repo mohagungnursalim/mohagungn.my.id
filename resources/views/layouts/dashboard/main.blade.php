abe12d0be5ca41d1693c399de0699bc2065f4090
mohagungnursalim <mohagungnursalim@gmail.com>
2025-09-01 14:30:19 +0800

    feat: menambahkan mode gelap/terang

--- resources/views/layouts/dashboard/main.blade.php
+++ resources/views/layouts/dashboard/main.blade.php
@@ -1,28 +1,37 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="en" x-data :class="$store.theme?.dark ? 'dark' : ''">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Dashboard</title>
 
-    <!-- Scripts -->
+    <style>[x-cloak]{display:none !important}</style>
+
+    {{-- Hindari flicker: set "dark" sebelum CSS di-load --}}
+    <script>
+        (() => {
+            try {
+                const dark = localStorage.getItem('dark') === 'true';
+                if (dark) document.documentElement.classList.add('dark');
+            } catch (_) {}
+        })();
+    </script>
+
+    <title>Dashboard</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @livewireStyles
 </head>
 
-<body class="dark:bg-gray-800">
+<body class="bg-white text-gray-900 dark:bg-gray-900 dark:text-gray-100">
 
-    {{-- Navbar --}}
+    {{-- Navbar (akan kita isi tombolnya) --}}
     @include('layouts.dashboard.navbar')
 
     {{-- Sidebar --}}
     @include('layouts.dashboard.sidebar')
 
     {{-- Konten --}}
-    <div class="p-4 sm:ml-64 dark:bg-gray-800">
-        <div class="p-4 border-2 border-white border-dashed rounded-lg dark:border-gray-100 mt-14">
+    <div class="p-4 sm:ml-64">
+        <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700 mt-14">
             {{ $slot }}
         </div>
     </div>
@@ -32,5 +41,4 @@
     @livewireScripts
     @stack('scripts')
 </body>
-
 </html>
