251423f3b7232fe233f7a559fb07040f8c015492
mohagungnursalim <mohagungnursalim@gmail.com>
2025-10-19 23:33:03 +0800

    fix: kategori kosong

--- resources/views/livewire/dashboard/categories.blade.php
+++ resources/views/livewire/dashboard/categories.blade.php
@@ -122,11 +122,11 @@
                                 </tr>
                                 @endforeach
 
-                                {{-- Jika kosong --}}
+                                {{-- Jika Data Kosong --}}
                                 @if($categories->isEmpty())
                                 <tr>
                                     <td colspan="5" class="px-6 py-4 text-center text-gray-500 dark:text-gray-400">
-                                        No categories found
+                                        No Categories found
                                     </td>
                                 </tr>
                                 @endif
