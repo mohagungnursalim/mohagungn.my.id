de434db43ebfea848e85d15334517c62f14c4893
mohagungnursalim <mohagungnursalim@gmail.com>
2026-05-25 13:31:00 +0800

    Fix: Bootstrap config

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => PermissionMiddleware::class,
             'role_or_permission' => RoleOrPermissionMiddleware::class,
         ]);
+         // 🔥 Ini kunci biar HTTPS kebaca walau lewat Cloudflare Tunnel
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
